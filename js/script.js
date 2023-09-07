6b67ffa9eb69461476395a645adaaf0bdecb7876
Maksymilian <max.szewczyk@interia.pl>
2023-09-07 22:28:59 +0200

    Created openPanel and closePanel function & added listeners

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // Top menu
-const addBtn = document.querySelector('.menu__buttons-add')
-const deleteBtn = document.querySelector('.menu__buttons-delete')
+const menu = document.querySelector('.menu') // main
+const addBtn = menu.querySelector('.menu__buttons-add')
+const deleteAllBtn = menu.querySelector('.menu__buttons-delete')
 // Note
-const noteArea = document.querySelector('.note-area')
+const noteArea = document.querySelector('.note-area') // main
 const deleteNoteBtn = noteArea.getElementsByClassName('note-area__header-delete')
 // Note panel
-const notePanel = document.querySelector('.note__panel')
+const notePanel = document.querySelector('.note__panel') // main
 const saveBtn = notePanel.querySelector('.save')
 const cancelBtn = notePanel.querySelector('.cancel')
 
@@ -13,5 +14,23 @@ const categoryPanel = notePanel.querySelector('#category')
 const textPanel = notePanel.querySelector('#text')
 const error = notePanel.querySelector('.note__panel-error')
 
-let selectedValue;
-let cardID = 0;
+let selectedValue
+let cardID = 0
+
+const openPanel = () => {
+	if (notePanel.style.display === 'flex') {
+		closePanel()
+	} else {
+		notePanel.style.display = 'flex'
+	}
+}
+
+const closePanel = () => {
+	notePanel.style.display = 'none'
+	textPanel.value = ''
+	error.style.visibility = 'hidden'
+	category.selectedIndex = 0
+}
+
+addBtn.addEventListener('click', openPanel)
+cancelBtn.addEventListener('click', openPanel)
